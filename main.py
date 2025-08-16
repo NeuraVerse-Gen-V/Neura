283a9f281cdb6af92d433abfa67668ea64769e64
Sylo <122664346+Sylo3285@users.noreply.github.com>
2025-08-16 23:08:05 +0530

    added interface for discord

--- main.py
+++ main.py
@@ -5,9 +5,26 @@ from model.tokenizer import BPETokenizer
 from utils.config import *
 #from vision import screen_reader,preprocess
 #from voice import stt,tts
+import discord_interface.discord_interface as discord_conector
 
-import time
+#-------------------------------------------------DISCORD SETUP-------------------------------------------------
+discord_bot = discord_conector.BotClient()
+bot = discord_bot.bot
 
+@bot.event
+async def on_message(message):
+    if message.author.bot:  # avoid replying to itself or other bots
+        return
+
+    print(f"Received: {message.content}")
+    output = generate(message.content)
+    print(f"Generated: {output}")
+
+    await message.reply(output)
+
+    # Ensure commands still work
+    await bot.process_commands(message)
+#-------------------------------------------------MODEL SETUP-------------------------------------------------
 tokenizer=BPETokenizer("gpt2")
 model=Transformer(src_pad_idx,trg_pad_idx,trg_sos_idx,eos_token,enc_voc_size,dec_voc_size,d_model,n_heads,ffn_hidden,n_layers,drop_prob,device)
 model.load_state_dict(torch.load("best_model.pt", map_location=device))
@@ -29,9 +46,10 @@ def vision():
 def speak():
     pass #code for TTS goes here
 
-a=time.time()
-output= generate(" me a story")
-b=time.time()
-print(output)
+def discord_int():
+    pass
 
-print(f"total time it took to generate one response: {b-a}")
+
+
+if __name__ == "__main__":
+    discord_bot.run()
